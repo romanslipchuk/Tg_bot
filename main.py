9285e793a2578582934fb87b5afd45081d9d9e26
Roman Slipchuk <roman.slipchuk@gmail.com>
2023-05-18 14:37:21 +0200

    add ui branch

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
+from telebot import types
+import requests
+from bs4 import BeautifulSoup
 from logs.auth_token import token
 
 bot = telebot.TeleBot(token)
 
 """
-TODO: 
-0. Add vendors buttons 
+TODO:
+0. Add vendors buttons
 1. Add request to Wates site
 2. Add parsing respond
 2-a Create docker image to run on server
@@ -15,21 +18,31 @@ TODO:
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f"Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b> "
-    bot.send_message(message.chat.id, mess, parse_mode='html')
+    mess = "Please select your HPLC vendor"
+    bot.send_message(message.chat.id, mess)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    waters = types.KeyboardButton("Waters")
+    agilent = types.KeyboardButton("Agilent")
+    thermo = types.KeyboardButton("Thermo")
+    shimadzu = types.KeyboardButton("Shimadzu")
+    markup.add(waters, agilent, thermo, shimadzu)
+    bot.send_message(message.chat.id, "Or visit google!", reply_markup=markup)
 
 
-@bot.message_handler()
+@bot.message_handler(content_types=['text'])
 def get_user_txt(message):
-    if message.text == 'Hello':
-        bot.send_message(message.chat.id, "Hello YOu too!", parse_mode='html')
-    elif message.text == 'id':
-        bot.send_message(message.chat.id, f"Your ID: {message.from_user.id}", parse_mode='html')
-    elif message.text == "photo":
-        photo = open('Antonio.png', 'rb')
-        bot.send_photo(message.chat.id, photo)
-    else:
-        bot.send_message(message.chat.id, "GOVORITE PO RUSSKI", parse_mode='html')
+    bot.send_message(message.chat.id, f"Using {message.text} database")
+
+
+@bot.message_handler(commands=["website"])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    website = types.KeyboardButton("WebSite")
+    start = types.KeyboardButton('Start')
+    stop = types.KeyboardButton('Stop')
+
+    markup.add(website, start, stop)
+    bot.send_message(message.chat.id, "Visit google!", reply_markup=markup)
 
 
 
